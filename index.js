563b68c59af011dffd0205809003f37fccc5f94a
Carlos Carvallo <carloscarvallo87@gmail.com>
2017-11-15 19:01:08 -0300

    new feature: export bundle to apigee when flows are updated

--- index.js
+++ index.js
@@ -4,6 +4,7 @@ const chokidar = require('chokidar')
 const path = require('path')
 const updateResources = require('./lib/update-resource-files')
 const updatePolicies = require('./lib/update-policies-files')
+const updateRevision = require('./lib/export-bundle')
 const args = require('./lib/arguments')
 
 const log = console.log.bind(console)
@@ -13,25 +14,36 @@ const config = configurer(args)
 
 // chokidar options
 var options = {
-  ignoreInitial: true,
-  ignored: /(^|[\/\\])\../
+    ignoreInitial: true,
+    ignored: /(^|[\/\\])\../
 }
 
-log(`Starting Watcher for \nProxy: ${config.api_name} \nRevision: ${config.api_revision} \nOrganization: ${config.api_organization}\nUser: ${config.apigee_username}` )
+log(`Starting Watcher for \nProxy: ${config.api_name} \nRevision: ${config.api_revision} \nOrganization: ${config.api_organization}\nUser: ${config.apigee_username}`)
 
 chokidar.watch(args.dir, options)
-  .on(args.event, (wPath, event) => {
-    var dirname = path.dirname(wPath)
+    .on(args.event, (wPath, event) => {
 
-    if (wPath.indexOf('/jsc') > 0) {
-      var jsFile = path.basename(wPath)
-      updateResources(config, dirname, jsFile)
-    }
+        var dirname = path.dirname(wPath)
 
-    if (wPath.indexOf('/policies') > 0) {
-      log('Updating policy: ', wPath)
-      var xmlFile = path.basename(wPath, '.xml')
-      log('file name', xmlFile)
-      updatePolicies(config, dirname, xmlFile)
-    }
-  })
+        if (wPath.indexOf('/jsc') > 0) {
+            var jsFile = path.basename(wPath)
+            updateResources(config, dirname, jsFile)
+        }
+
+        if (wPath.indexOf('/policies') > 0) {
+            log('Updating policy: ', wPath)
+            var xmlFile = path.basename(wPath, '.xml')
+            log('file name', xmlFile)
+            updatePolicies(config, dirname, xmlFile)
+        }
+
+        if (wPath.indexOf('/proxies') > 0) {
+            log('Exporting bundle: ', wPath)
+            var xmlFile = path.basename(wPath, '.xml')
+            var currentDir = path.dirname(wPath)
+            log('file name', xmlFile)
+            var dirToExport = currentDir.replace('/proxies', "");
+            log('dir to export', dirToExport)
+            updateRevision(config, dirToExport)
+        }
+    })
